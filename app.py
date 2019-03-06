dd573ff5d87bced7685460f30d144f6041d509df
Ismael Garcia <igarcia@localhost.localdomain>
2019-03-06 13:31:37 -0500

    add heroku required files for running

--- app.py
+++ app.py
@@ -38,6 +38,6 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    app.run(port=5000, debug=True) 
 
 
